f7cc9358d88f672529fd33029f5bcec75ed2f269
李帅 <lishuai02@meituan.com>
2014-08-11 10:53:32 +0800

    Initial Commit

---
+++ FontTool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FontTool
+//
+//  Created by 李帅 on 8/11/14.
+//  Copyright (c) 2014 hehuababy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FontTool/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FontTool
+//
+//  Created by 李帅 on 8/11/14.
+//  Copyright (c) 2014 hehuababy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
